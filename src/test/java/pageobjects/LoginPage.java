d61e4ccbf7739224cd7ce18f4a0969fc840ac2a3
Daniel Porto <danielsalesporto@gmail.com>
2022-08-18 11:57:36 -0300

    fix folders

--- src/test/java/pageobjects/LoginPage.java
+++ src/test/java/pageobjects/LoginPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
-import Utils.MetodosUteis;
+import utils.MetodosUteis;
 
 public class LoginPage extends MetodosUteis {
 
